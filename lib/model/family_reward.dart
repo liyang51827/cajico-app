a1c52d04676b240fef9618b6ccd7265f786c7f7f
Takahashi Katsunori <katsutennis.916@gmail.com>
2022-12-11 16:22:00 +0900

    [fix]noteのつなぎこみ追加

--- lib/model/family_reward.dart
+++ lib/model/family_reward.dart
@@ -8,18 +8,21 @@ class FamilyReward {
     required this.smallRewardDifferencePoint,
     required this.smallRewardRating,
     required this.smallRewardIsRequesting,
+    required this.smallRewardNote,
     required this.middleRewardName,
     required this.middleRewardPoint,
     required this.middleRewardOwnedPoint,
     required this.middleRewardDifferencePoint,
     required this.middleRewardRating,
     required this.middleRewardIsRequesting,
+    required this.middleRewardNote,
     required this.largeRewardName,
     required this.largeRewardPoint,
     required this.largeRewardOwnedPoint,
     required this.largeRewardDifferencePoint,
     required this.largeRewardRating,
     required this.largeRewardIsRequesting,
+    required this.largeRewardNote,
   });
 
   final String userName;
@@ -30,18 +33,21 @@ class FamilyReward {
   final int smallRewardDifferencePoint;
   final double smallRewardRating;
   final bool smallRewardIsRequesting;
+  final String smallRewardNote;
   final String middleRewardName;
   final int middleRewardPoint;
   final int middleRewardOwnedPoint;
   final int middleRewardDifferencePoint;
   final double middleRewardRating;
   final bool middleRewardIsRequesting;
+  final String middleRewardNote;
   final String largeRewardName;
   final int largeRewardPoint;
   final int largeRewardOwnedPoint;
   final int largeRewardDifferencePoint;
   final double largeRewardRating;
   final bool largeRewardIsRequesting;
+  final String largeRewardNote;
 
   static FamilyReward fromJson(Map<String, dynamic> json) {
     return FamilyReward(
@@ -53,18 +59,21 @@ class FamilyReward {
       smallRewardDifferencePoint: json['smallReward']['differencePoint'],
       smallRewardRating: json['smallReward']['rating'],
       smallRewardIsRequesting: json['smallReward']['isRequesting'],
+      smallRewardNote: json['smallReward']['note'],
       middleRewardName:  json['middleReward']['name'],
       middleRewardPoint: json['middleReward']['rewardPoint'],
       middleRewardOwnedPoint: json['middleReward']['ownedPoint'],
       middleRewardDifferencePoint: json['middleReward']['differencePoint'],
       middleRewardRating: json['middleReward']['rating'],
       middleRewardIsRequesting: json['middleReward']['isRequesting'],
+      middleRewardNote: json['middleReward']['note'],
       largeRewardName:  json['largeReward']['name'],
       largeRewardPoint: json['largeReward']['rewardPoint'],
       largeRewardOwnedPoint: json['largeReward']['ownedPoint'],
       largeRewardDifferencePoint: json['largeReward']['differencePoint'],
       largeRewardRating: json['largeReward']['rating'],
       largeRewardIsRequesting: json['largeReward']['isRequesting'],
+      largeRewardNote: json['largeReward']['note'],
     );
   }
 }
