43be0877e2d614019b6f50390c777e9fa66f463f
Takahashi Katsunori <katsutennis.916@gmail.com>
2022-12-11 21:46:12 +0900

    [fix]お知らせアイコンにバッチを追加

--- lib/ui/widget/notification.dart
+++ lib/ui/widget/notification.dart
@@ -1,3 +1,5 @@
+import 'package:badges/badges.dart';
+import 'package:cajico_app/ui/common/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
@@ -11,8 +13,15 @@ class NotificationAction extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: IconButton(
-          onPressed: () => Get.to(() => const NotificationView()),
-          icon: const Icon(LineIcons.bell, size: 30)),
+        onPressed: () => Get.to(() => const NotificationView()),
+        icon: Badge(
+          position: const BadgePosition(start: 15, top: -5),
+          badgeContent: const Text('3', style: TextStyle(color: Colors.white)),
+          elevation: 0,
+          badgeColor: secondaryColor,
+          child: const Icon(LineIcons.bell, size: 30),
+        ),
+      )
     );
   }
 }
