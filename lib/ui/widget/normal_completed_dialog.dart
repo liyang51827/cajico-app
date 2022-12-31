6a754703bd54548bc874a48becb6ec1cc6827e26
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-01-01 03:15:31 +0900

    [fix]問い合わせ時にunreadCountを取得するようにする

--- lib/ui/widget/normal_completed_dialog.dart
+++ lib/ui/widget/normal_completed_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:cajico_app/ui/widget/primary_small_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/home_view_controller.dart';
 
 class NormalCompletedDialog extends StatelessWidget {
   const NormalCompletedDialog({super.key, required this.message});
@@ -8,6 +10,8 @@ class NormalCompletedDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final homeController = Get.put(HomeViewController());
     return SimpleDialog(
       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
       title: Text(message,
@@ -18,6 +22,7 @@ class NormalCompletedDialog extends StatelessWidget {
           child: const PrimarySmallButton(text: 'OK'),
           onPressed: () {
             Navigator.pop(context);
+            homeController.fetchData();
           },
         )
       ],
