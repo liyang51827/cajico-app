0b0df269efdb207c21f8f4dafbde329786ed3057
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-01-12 01:36:37 +0900

    [fix]ローディングを追加

--- lib/ui/view/my_page_view.dart
+++ lib/ui/view/my_page_view.dart
@@ -5,12 +5,14 @@ import 'package:cajico_app/ui/controller/my_page_view_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import '../widget/loading_stack.dart';
 
 class MyPageView extends StatelessWidget {
   const MyPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(MyPageViewController());
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.black54),
@@ -19,71 +21,77 @@ class MyPageView extends StatelessWidget {
         backgroundColor: Colors.white,
         titleTextStyle: const TextStyle(fontSize: 22),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Obx(() {
-          final controller = Get.put(MyPageViewController());
-          final user = controller.user();
-          final formatter = NumberFormat("#,###");
-          if (user == null) {
-            return const SizedBox();
-          }
-          return Container(
-            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              border: Border.all(color: gray5),
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Column(
+      body: GetLoadingStack<MyPageViewController>(
+        child: RefreshIndicator(
+          color: primaryColor,
+          onRefresh: controller.fetchData,
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            padding: const EdgeInsets.all(16),
+            child: Obx(() {
+              final user = controller.user();
+              final formatter = NumberFormat("#,###");
+              if (user == null) {
+                return const SizedBox();
+              }
+              return Container(
+                padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 36),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(color: gray5),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
                   children: [
-                    Text(
-                      '${user.userName}さん',
-                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    verticalSpaceMediumLarge,
-                    SizedBox(
-                      height: 80,
-                      child: CachedNetworkImage(
-                        imageUrl: user.iconUrl,
-                        placeholder: (context, url) => const Center(
-                          child: CircularProgressIndicator(color: primaryColor),
+                    Column(
+                      children: [
+                        Text(
+                          '${user.userName}さん',
+                          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                      ),
+                        verticalSpaceMediumLarge,
+                        SizedBox(
+                          height: 80,
+                          child: CachedNetworkImage(
+                            imageUrl: user.iconUrl,
+                            placeholder: (context, url) => const Center(
+                              child: CircularProgressIndicator(color: primaryColor),
+                            ),
+                          ),
+                        ),
+                        verticalSpaceMediumLarge,
+                        const Text('保有ポイント', style: TextStyle(fontWeight: FontWeight.bold)),
+                        verticalSpaceSmall,
+                        Text(
+                          '${formatter.format(user.ownedPoint)}P',
+                          style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor,
+                          ),
+                        ),
+                        verticalSpaceLarge,
+                      ],
                     ),
-                    verticalSpaceMediumLarge,
-                    const Text('保有ポイント', style: TextStyle(fontWeight: FontWeight.bold)),
-                    verticalSpaceSmall,
-                    Text(
-                      '${formatter.format(user.ownedPoint)}P',
-                      style: const TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: primaryColor,
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _MyPageMenu(menu: 'ファミリーネーム', value: '${user.familyName}家'),
+                        _MyPageMenu(menu: 'ファミリーコード', value: user.familyCode),
+                        _MyPageMenu(menu: '続柄', value: user.position),
+                        _MyPageMenu(menu: 'メールアドレス', value: user.email),
+                        _MyPageMenu(menu: '誕生日', value: user.birthday),
+                        _MyPageMenu(menu: '本日の獲得P', value: '${formatter.format(user.todayPoint)}P'),
+                        _MyPageMenu(menu: '累計獲得P', value: '${formatter.format(user.totalPoint)}P'),
+                      ],
                     ),
-                    verticalSpaceLarge,
                   ],
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _MyPageMenu(menu: 'ファミリーネーム', value: '${user.familyName}家'),
-                    _MyPageMenu(menu: 'ファミリーコード', value: user.familyCode),
-                    _MyPageMenu(menu: '続柄', value: user.position),
-                    _MyPageMenu(menu: 'メールアドレス', value: user.email),
-                    _MyPageMenu(menu: '誕生日', value: user.birthday),
-                    _MyPageMenu(menu: '本日の獲得P', value: '${formatter.format(user.todayPoint)}P'),
-                    _MyPageMenu(menu: '累計獲得P', value: '${formatter.format(user.totalPoint)}P'),
-                  ],
-                ),
-              ],
-            ),
-          );
-        }),
+              );
+            }),
+          ),
+        ),
       ),
     );
   }
