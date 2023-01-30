427f3d940088d99945df7d48a51376489e2587ce
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-01-31 00:00:13 +0900

    [fix]家族登録画面の実装

--- lib/ui/view/register_family_view.dart
+++ lib/ui/view/register_family_view.dart
@@ -1,6 +1,5 @@
 import 'package:cajico_app/ui/common/app_color.dart';
 import 'package:cajico_app/ui/controller/email_send_view_controller.dart';
-import 'package:cajico_app/ui/view/register_family_view.dart';
 import 'package:cajico_app/ui/widget/primary_button.dart';
 import 'package:cajico_app/ui/widget/progress_bar.dart';
 import 'package:flutter/material.dart';
@@ -9,10 +8,9 @@ import '../common/ui_helper.dart';
 import '../widget/cajico_text_form_field.dart';
 import '../widget/loading_stack.dart';
 
-class EmailSendView extends StatelessWidget {
-  EmailSendView({super.key, required this.type});
+class RegisterFamilyView extends StatelessWidget {
+  RegisterFamilyView({super.key});
 
-  final String type;
   final focusNode = FocusNode();
 
   @override
@@ -28,7 +26,7 @@ class EmailSendView extends StatelessWidget {
           appBar: AppBar(
             iconTheme: const IconThemeData(color: Colors.black54),
             centerTitle: true,
-            title: const Text('メールアドレス登録', style: TextStyle(color: gray2)),
+            title: const Text('家族登録', style: TextStyle(color: gray2)),
             backgroundColor: Colors.white,
             titleTextStyle: const TextStyle(fontSize: 22),
           ),
@@ -37,34 +35,44 @@ class EmailSendView extends StatelessWidget {
               padding: const EdgeInsets.only(right: 16, left: 16, top: 16, bottom: 24),
               child: Column(
                 children: [
-                  const ProgressBar(step: 1),
+                  const ProgressBar(step: 2),
                   verticalSpaceLarge,
-                  const Text('メールアドレスを入力してください', textAlign: TextAlign.center),
+                  const Text('家族の名前と\n他の方が家族に参加するための\n家族コードを設定してください', textAlign: TextAlign.center),
                   verticalSpaceLarge,
                   CajicoTextFormField(
-                    label: 'メールアドレス',
+                    label: '家族名',
                     initValue: '',
                     onChanged: (value) => controller.email.value = value,
                     validator: (value) => controller.validateInputEmailData(value).message,
                   ),
-                  verticalSpaceLarge,
-                  PrimaryButton(
-                    label: 'STEP2へ',
-                    onPressed: () => Get.to(() => RegisterFamilyView()),
-                  )
+                  verticalSpaceMedium,
+                  CajicoTextFormField(
+                    label: '家族コード',
+                    initValue: '',
+                    onChanged: (value) => controller.email.value = value,
+                    validator: (value) => controller.validateInputEmailData(value).message,
+                  ),
+                  verticalSpaceSmall,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 4),
+                    child: Text(
+                      '※家族コードは8文字以上20文字以下の半角英数字でご入力ください。',
+                      style: TextStyle(color: gray4, fontSize: 13),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-          bottomNavigationBar: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            child: Obx(() => PrimaryButton(
-                label: '送信する',
-                isValid: controller.isSendButtonValid,
-                onPressed: () {
-                  controller.onTapSendEmail(type: type);
-                })),
-          ),
+          // bottomNavigationBar: Container(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          //   child: Obx(() => PrimaryButton(
+          //       label: '次へ',
+          //       isValid: controller.isSendButtonValid,
+          //       onPressed: () {
+          //         controller.onTapSendEmail(type: type);
+          //       })),
+          // ),
         ),
       ),
     );
