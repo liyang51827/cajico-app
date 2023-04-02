932a20343674b316b3f3220b20d3066ddde94f4b
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-04-03 01:33:19 +0900

    [fix]スケジュール画面をモックベースで作成

--- lib/ui/view/schedule_view.dart
+++ lib/ui/view/schedule_view.dart
@@ -1,6 +1,5 @@
 import 'package:cajico_app/ui/common/app_color.dart';
 import 'package:flutter/material.dart';
-import '../widget/background.dart';
 import '../widget/footer.dart';
 import '../widget/header.dart';
 import '../widget/home_drawer.dart';
@@ -27,14 +26,55 @@ class ScheduleView extends StatelessWidget {
           height: MediaQuery.of(context).size.height - 143,
           child: SfCalendar(
             view: CalendarView.day,
+            headerDateFormat: 'yyyy年M月',
             timeSlotViewSettings: const TimeSlotViewSettings(
               timeIntervalHeight: 80,
               timeFormat: 'HH:mm',
             ),
+            dataSource: _getCalendarDataSource(),
           ),
         ),
       ),
       bottomNavigationBar: const Footer(),
     );
+  }
+}
+
+List<Appointment> _getAppointments(DateTime date) {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.add(Appointment(
+      startTime: DateTime(date.year, date.month, date.day, 10, 0, 0),
+      endTime: DateTime(date.year, date.month, date.day, 10, 15, 0),
+      subject: '家事１',
+      color: primaryColor.withOpacity(0.5)));
+  appointments.add(Appointment(
+      startTime: DateTime(date.year, date.month, date.day, 11, 0, 0),
+      endTime: DateTime(date.year, date.month, date.day, 11, 30, 0),
+      subject: '家事２',
+      color: primaryColor));
+  appointments.add(Appointment(
+      startTime: DateTime(date.year, date.month, date.day, 12, 0, 0),
+      endTime: DateTime(date.year, date.month, date.day, 12, 45, 0),
+      subject: '家事２',
+      color: primaryColor.withOpacity(0.5)));
+  return appointments;
+}
+
+_DataSource _getCalendarDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  DateTime today = DateTime.now();
+  DateTime rangeStartDate = DateTime(today.year, today.month, today.day - 7, 0, 0, 0);
+  DateTime rangeEndDate = DateTime(today.year, today.month, today.day + 7, 0, 0, 0);
+  for (DateTime i = rangeStartDate; i.isBefore(rangeEndDate); i = i.add(Duration(days: 1))) {
+    List<Appointment> dailyAppointments = _getAppointments(i);
+    appointments.addAll(dailyAppointments);
+  }
+
+  return _DataSource(appointments);
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
