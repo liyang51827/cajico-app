30514c005eea38baf7c75690cabd88f85fcaa734
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-04-11 00:08:26 +0900

    [fix]obxの囲みを消去

--- lib/ui/view/schedule_create_view.dart
+++ lib/ui/view/schedule_create_view.dart
@@ -10,6 +10,8 @@ import '../widget/date_picker_form.dart';
 import '../widget/time_picker_form.dart';
 import '../widget/primary_button.dart';
 
+const List<String> intervals = <String>['1', '2', '3'];
+
 class ScheduleCreateView extends StatelessWidget {
   ScheduleCreateView({
     super.key,
@@ -42,78 +44,64 @@ class ScheduleCreateView extends StatelessWidget {
           body: GetLoadingStack<ScheduleViewController>(
             child: SingleChildScrollView(
               padding: const EdgeInsets.all(16),
-              child: Obx(
-                () => Column(
-                  children: [
-                    verticalSpaceLarge,
-                    DatePickerForm(
-                      labelText: '日付',
-                      firstDate: DateTime(today.year - 1, today.month, today.day),
-                      lastDate: DateTime(today.year + 1, today.month, today.day),
-                      onChange: (value) => scheduleInfo.date.value = outputFormat.format(value!),
-                      initialDate: selectedDate!,
-                      showInitialDate: true,
-                    ),
-                    verticalSpaceMedium,
-                    TimePickerForm(
-                      labelText: '開始時間',
-                      onChange: (value) => scheduleInfo.date.value =
-                          DateFormat.Hm().format(DateTime(2023, 4, 10, value!.hour, value.minute)),
-                      initialTime: TimeOfDay.fromDateTime(today),
-                      showInitialDate: true,
-                    ),
-                    verticalSpaceMedium,
-                    TimePickerForm(
-                      labelText: '終了時間',
-                      onChange: (value) => scheduleInfo.date.value =
-                          DateFormat.Hm().format(DateTime(2023, 4, 10, value!.hour, value.minute)),
-                      initialTime: TimeOfDay.fromDateTime(today.add(const Duration(minutes: 10))),
-                      showInitialDate: true,
-                    ),
-                    verticalSpaceMedium,
-                    CajicoDropDown(
-                      onChanged: (value) => scheduleInfo.repeatRule.value = value!,
-                      items: const [
-                        DropdownMenuItem(value: 999, child: Text('繰り返さない')),
-                        DropdownMenuItem(value: 0, child: Text('日')),
-                        DropdownMenuItem(value: 1, child: Text('週')),
-                        DropdownMenuItem(value: 2, child: Text('月')),
-                      ],
-                      labelText: '繰り返し',
-                    ),
-                    verticalSpaceMedium,
-                    scheduleInfo.repeatRule() == 0 || scheduleInfo.repeatRule() == 1
-                        ? CajicoDropDown(
-                            onChanged: (value) => scheduleInfo.repeatInterval.value = value!,
-                            items: const [
-                              DropdownMenuItem(value: 1, child: Text('1')),
-                              DropdownMenuItem(value: 2, child: Text('2')),
-                              DropdownMenuItem(value: 3, child: Text('3')),
-                            ],
-                            labelText: '間隔',
-                          )
-                        : scheduleInfo.repeatRule() == 2
-                            ? CajicoDropDown(
-                                onChanged: (value) => scheduleInfo.repeatInterval.value = value!,
-                                items: const [
-                                  DropdownMenuItem(value: 1, child: Text('1')),
-                                ],
-                                labelText: '間隔',
-                              )
-                            : const SizedBox(),
-                    verticalSpaceMedium,
-                    scheduleInfo.repeatRule() != 999
-                        ? DatePickerForm(
-                            hintText: '繰り返し終了日',
-                            initialDate: DateTime(today.year, today.month, today.day),
-                            firstDate: DateTime(today.year, today.month, today.day),
-                            lastDate: DateTime(today.year + 1, today.month, today.day),
-                            onChange: (value) =>
-                                scheduleInfo.repeatEndDate.value = outputFormat.format(value!),
-                          )
-                        : const SizedBox(),
-                  ],
-                ),
+              child: Column(
+                children: [
+                  verticalSpaceLarge,
+                  DatePickerForm(
+                    labelText: '日付',
+                    firstDate: DateTime(today.year - 1, today.month, today.day),
+                    lastDate: DateTime(today.year + 1, today.month, today.day),
+                    onChange: (value) => scheduleInfo.date.value = outputFormat.format(value!),
+                    initialDate: selectedDate!,
+                    showInitialDate: true,
+                  ),
+                  verticalSpaceMedium,
+                  TimePickerForm(
+                    labelText: '開始時間',
+                    onChange: (value) => scheduleInfo.date.value =
+                        DateFormat.Hm().format(DateTime(2023, 4, 10, value!.hour, value.minute)),
+                    initialTime: TimeOfDay.fromDateTime(today),
+                    showInitialDate: true,
+                  ),
+                  verticalSpaceMedium,
+                  TimePickerForm(
+                    labelText: '終了時間',
+                    onChange: (value) => scheduleInfo.date.value =
+                        DateFormat.Hm().format(DateTime(2023, 4, 10, value!.hour, value.minute)),
+                    initialTime: TimeOfDay.fromDateTime(today.add(const Duration(minutes: 10))),
+                    showInitialDate: true,
+                  ),
+                  verticalSpaceMedium,
+                  CajicoDropDown(
+                    onChanged: (value) => scheduleInfo.repeatRule.value = value!,
+                    items: const [
+                      DropdownMenuItem(value: 999, child: Text('繰り返さない')),
+                      DropdownMenuItem(value: 0, child: Text('日')),
+                      DropdownMenuItem(value: 1, child: Text('週')),
+                      DropdownMenuItem(value: 2, child: Text('月')),
+                    ],
+                    labelText: '繰り返し',
+                  ),
+                  verticalSpaceMedium,
+                  CajicoDropDown(
+                    onChanged: (value) => scheduleInfo.repeatInterval.value = value!,
+                    items: const [
+                      DropdownMenuItem(value: 1, child: Text('1')),
+                      DropdownMenuItem(value: 2, child: Text('2')),
+                      DropdownMenuItem(value: 3, child: Text('3')),
+                    ],
+                    labelText: '間隔',
+                  ),
+                  verticalSpaceMedium,
+                  DatePickerForm(
+                    labelText: '繰り返し終了日',
+                    initialDate: DateTime(today.year, today.month, today.day),
+                    firstDate: DateTime(today.year, today.month, today.day),
+                    lastDate: DateTime(today.year + 1, today.month, today.day),
+                    onChange: (value) =>
+                        scheduleInfo.repeatEndDate.value = outputFormat.format(value!),
+                  ),
+                ],
               ),
             ),
           ),
